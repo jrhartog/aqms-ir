6db36fbf9204ebdee66e3cc9e7d144bdd7f7dc24
Renate Hartog <jrhartog@gmail.com>
2017-11-13 09:22:08 -0800

    increased version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="aqms-ir", 
-    version="0.0.1",
+    version="0.0.2",
     description="translation between obspy Inventory object and AQMS schema",
     url="http://github.com/jrhartog/aqms-ir",
     author="Renate Hartog",
